5f80886e7961062aa119d32f453c6a90cce1d80a
Hari Madhav Jha <madhav1993hari@gmail.com>
2017-07-18 12:11:25 +0530

    Add Js file

---
+++ js/app.js
@@ -0,0 +1,36 @@
+var accordion = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < accordion.length; i++) {
+    accordion[i].onclick = function(){
+        
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+};
+
+var sideMenu = document.getElementById("sideMenu");
+function clickMenu(){
+   sideMenu.classList.toggle("toggleMenu");
+};
+
+function openLearn(evt, task){
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("howTo");
+	for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tab");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(task).style.display = "block";
+    evt.currentTarget.className += " active";
+};
+
+document.getElementById("defaultOpen").click();
+
