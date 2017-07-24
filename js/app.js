e0a4fcfc2d3d25e1cd0ae1c6d3baeca331879314
Hari Madhav Jha <madhav1993hari@gmail.com>
2017-07-24 16:50:09 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -20,25 +20,3 @@ function clickMenu(){
 function closeMenu() {
 	sideMenu.style.width = "0";
 }
-
-function openLearn(evt, task){
-	var i, tabcontent, tablinks;
-	tabcontent = document.getElementsByClassName("howTo");
-	for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    tablinks = document.getElementsByClassName("tab");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(task).style.display = "block";
-    evt.currentTarget.className += " active";
-};
-
-var clickthis = document.getElementById("defaultOpen");
-
-if (clickthis) {
-    clickthis.click();
-}
-
